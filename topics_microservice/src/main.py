643ce4136486034bcf178231a6210a6a526fced1
Data Name ID <data.name.id@yandex.ru>
2024-08-08 11:07:18 +0300

    fix cors

--- topics_microservice/src/main.py
+++ topics_microservice/src/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routers import permissions, subscriptions, topics
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
+
     app.include_router(topics.topic_router)
     app.include_router(permissions.permission_router)
     app.include_router(subscriptions.subscription_router)
